52a80ddbe2702914e9dc00de8b141d44dbd0783e
Javier Serrano Gomez <javier.sego04@gmail.com>
2025-12-01 10:06:49 +0100

    Created Spring Project

---
+++ tagme-store-back/src/main/java/com/tagme/tagme_store_back/TagmeStoreBackApplication.java
@@ -0,0 +1,13 @@
+package com.tagme.tagme_store_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagmeStoreBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagmeStoreBackApplication.class, args);
+	}
+
+}
---
+++ tagme-store-back/src/test/java/com/tagme/tagme_store_back/TagmeStoreBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tagme.tagme_store_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagmeStoreBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
