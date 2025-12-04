0fb6f607087d2964821667f0bc7b07b334e7b0d5
Javier Serrano Gomez <javier.sego04@gmail.com>
2025-12-04 11:54:02 +0100

    feat(https://github.com/Tag-Me-DAW2/sprints/issues/14): Añadido UserJpaEntity

--- src/main/java/com/tagme/tagme_store_back/persistence/dao/jpa/entity/UserJpaEntity.java
+++ src/main/java/com/tagme/tagme_store_back/persistence/dao/jpa/entity/UserJpaEntity.java
@@ -1,4 +1,104 @@
 package com.tagme.tagme_store_back.persistence.dao.jpa.entity;
 
-public class UserJpaEntity {
+import com.tagme.tagme_store_back.domain.model.UserRole;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class UserJpaEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+    @Column(name = "password", nullable = false)
+    private String password;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    private String phone;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private UserRole role;
+    private LocalDateTime createdAt;
+
+    public UserJpaEntity() {}
+
+    public UserJpaEntity(String email, String password, String firstName, String lastName, String phone, UserRole role, LocalDateTime createdAt) {
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.role = role;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
