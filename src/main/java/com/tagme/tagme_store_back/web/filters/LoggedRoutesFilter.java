b9c4895f9d20915c8f8e2bf645391498b409e73e
Javier Serrano Gómez <javier.sego04@gmail.com>
2025-12-10 23:37:25 +0100

    Feat(https://github.com/Tag-Me-DAW2/sprints/issues/31): Creado filtro para la comprobración de rutas que necesitan que estes logeado

--- src/main/java/com/tagme/tagme_store_back/web/filters/LoggedRoutesFilter.java
+++ src/main/java/com/tagme/tagme_store_back/web/filters/LoggedRoutesFilter.java
@@ -1,7 +1,6 @@
 package com.tagme.tagme_store_back.web.filters;
 
 import com.tagme.tagme_store_back.controller.webModel.response.UserResponse;
-import com.tagme.tagme_store_back.domain.model.UserRole;
 import com.tagme.tagme_store_back.web.context.AuthContext;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,23 +9,34 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
-@Order(3)
-public class AdminFilter implements Filter {
+@Order(2)
+public class LoggedRoutesFilter implements Filter {
+    private final List<String> protectedRoutes = List.of(
+            "/admin"
+    );
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
 
         String path = httpServletRequest.getRequestURI();
-        if (path.startsWith("/admin")) {
-            UserResponse user = AuthContext.getUser();
 
-            if (!user.role().equals(UserRole.ADMIN)) {
-                httpServletResponse.sendError(HttpServletResponse.SC_FORBIDDEN, "Forbidden");
-                return;
-            }
+        boolean serveProtectedRoute = protectedRoutes.stream().anyMatch(path::startsWith);
+
+        if (!serveProtectedRoute) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        UserResponse user = AuthContext.getUser();
+
+        if (user == null) {
+            httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+            return;
         }
 
         chain.doFilter(request, response);
