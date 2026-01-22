d9db84ab3169126968b846c2732d045dd7a6c686
Javier Serrano Gomez <javier.sego04@gmail.com>
2026-01-22 13:07:18 +0100

    Seguir

---
+++ src/main/java/com/tagme/tagme_store_back/domain/service/CartService.java
@@ -0,0 +1,12 @@
+package com.tagme.tagme_store_back.domain.service;
+
+import com.tagme.tagme_store_back.domain.dto.OrderDto;
+import com.tagme.tagme_store_back.domain.model.OrderStatus;
+
+public interface CartService {
+    void clearCart(Long userId);
+    void createCart(Long userId);
+    OrderDto getActiveCart(Long userId);
+    OrderStatus getCartStatus(Long userId);
+    void updateCart(OrderDto orderDto);
+}
